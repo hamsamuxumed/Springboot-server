4e2a98482bf74c235207cee0fb0ded5c88118202
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2023-02-14 14:36:55 +0000

    add test funtion

---
+++ README.md
@@ -0,0 +1,1 @@
+# Springboot-server
---
+++ src/main/java/com/hamsaDemo/main.java
@@ -0,0 +1,27 @@
+package com.hamsaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class main {
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+@GetMapping("/")
+    public GreetResponse hello() {
+    return new GreetResponse("This server is live", List.of("java", "JavaScript", "python"), new Person("Massan"));
+}
+
+    record Person(String name){}
+    record GreetResponse(String greet, List<String> favprogName, Person person) {}
+public String test(String name){
+        return name;
+}
+}
+
